8b25c58022a22b25c36bd31723b6e748b4a852fd
Ahno Chen <ahnochen1029@gmail.com>
2020-11-25 20:47:44 +0800

    user password and unique email

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,17 +28,6 @@ const userController = {
       })
     }
   }
-
-  // signUp: (req, res) => {
-  //   console.log(req.body)
-  //   User.create({
-  //     name: req.body.name,
-  //     email: req.body.email,
-  //     password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-  //   }).then(user => {
-  //     return res.redirect('/signin')
-  //   })
-  // }
 }
 
 module.exports = userController
