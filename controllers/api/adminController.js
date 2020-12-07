dd37c092f8ce5ad90421ee8d2ee0bd1bf7c59f6a
Ahno Chen <ahnochen1029@gmail.com>
2020-12-07 22:06:15 +0800

    refactor adminController.getRestaurants

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -2,10 +2,12 @@ const db = require('../../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const adminServices = require('../../services/adminServices')
+
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true, nest: true, include: [Category] }).then(restaurants => {
-      return res.json({ restaurants: restaurants })
+    adminServices.getRestaurants(req, res, data => {
+      return res.json(data)
     })
   }
 }
